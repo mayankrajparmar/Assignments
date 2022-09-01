cb0fc89d135cad2afd2493bfc5f3d82ff47f18b0
Mayank Raj Parmar <mayankrajparmar@gmail.com>
2022-09-01 23:38:54 +0530

    This is First Assignment

---
+++ Assignments-1.py
@@ -0,0 +1,53 @@
+                                 #Assignment-1: Warm-up with python
+
+# 1. Write a command to get the Python version you are using.
+
+from email.headerregistry import ParameterizedMIMEHeader
+from inspect import Parameter
+import sys
+print(sys.version)
+
+# 2. Write a python script to print Hello Python on the screen directly in the command line.
+
+print("Hello Python")
+
+# 3. Write a python script and store it in a file and execute the file from the command line.
+
+# For execute file from the command line first we have to go file location in command line then write python Filename.py
+# python Assignment-1.py
+
+# 4. Write a python script to print Hello Python on the screen using visual studio code.
+
+print("Hello Python")
+
+# 5. Write a python script to print Hello on the first line and Python on the second line.
+
+print("Hello","Python",sep='\n')
+
+# 6. Write a python script to print “MySirG” on the screen
+
+print("\"MySirG\"")
+
+# 7. Write a python script to print “Teacher’s Day” on the screen
+
+print("\"Teacher's Day\"")
+
+# 8. Write a python script to print the value of a variable. Variable contains your name as data.
+
+Name = "Mayank Raj Parmar"
+print(Name)
+
+# 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+
+Name = "Mayank"
+Age = 20
+Qualifications = "B.Tech"
+Years_of_experience = 0
+print(Name,Age,Qualifications,Years_of_experience)
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable contains integer values.
+
+a = 1
+b = 2
+c = 3
+print(a,b,c,sep='\n')
