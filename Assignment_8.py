e3584e7d59fc527fa05b291361920ada413005b3
Mayank Raj Parmar <mayankrajparmar@gmail.com>
2024-02-03 12:23:16 +0530

    Assignment_8 Added

---
+++ Assignment_8.py
@@ -0,0 +1,61 @@
+# Assignment-8: While Loop
+
+# 1. Write a python script to print MySirG 5 times on the screen
+i = 1
+while i <= 5:
+    print("MySirG")
+    i+=1
+
+# 2. Write a python script to print first 10 natural numbers
+i = 1
+while i <= 10:
+    print(i,end=" ")
+    i+=1
+
+# 3. Write a python script to print first 10 natural numbers in reverse order
+i = 1
+while i <= 10:
+    print(11-i,end=" ")
+    i+=1
+
+# 4. Write a python script to print first 10 odd natural numbers
+i = 0
+while i <= 9:
+    print(2*i+1,end=" ")
+    i+=1 
+
+# 5. Write a python script to print first 10 odd natural numbers in reverse order
+i = 9
+while i >= 0:
+    print(2*i+1,end=" ")
+    i-=1
+
+# 6. Write a python script to print first 10 even natural numbers
+i = 1
+while i <= 10:
+    print(2*i,end=" ")
+    i+=1
+
+# 7. Write a python script to print first 10 even natural numbers in reverse order
+i = 10
+while i >= 1:
+    print(2*i,end=" ")
+    i-=1
+
+# 8. Write a python script to print squares of first 10 natural numbers
+i = 1
+while i <= 10:
+    print(i*i,end=" ")
+    i+=1
+
+# 9. Write a python script to print cubes of first 10 natural numbers
+i = 1
+while i <= 10:
+    print(i**3,end=" ")
+    i+=1
+
+# 10. Write a python script to print first 10 multiples of 5
+i = 1
+while i <= 10:
+    print(5*i,end=" ")
+    i+=1
